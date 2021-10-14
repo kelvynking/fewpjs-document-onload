b2fe9482ee2bc035eb335d2fe887da0a4a3a5d52
kelvynking <kelvyn.m.king@gmail.com>
2021-10-14 14:35:21 +1100

    Added an event listener to change text on the page when DOM content has loaded.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+const text = document.getElementById("text");
+
+document.addEventListener("DOMContentLoaded", function () {
+    text.innerHTML = "This is really cool!";
+})
+
